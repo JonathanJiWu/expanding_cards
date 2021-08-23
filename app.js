55f782a4d98c38874814964f31de2c22703f71c4
YOUR NAME <62856817+AllisOrder@users.noreply.github.com>
2021-08-22 21:11:22 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const panels= document.querySelectorAll('.panel')
+console.log(panels[0])
+panels.forEach((panel)=>{
+    panel.addEventListener('click',()=>{
+        removeActiveClassess()
+        panel.classList.add('active')
+    })
+})
+function removeActiveClassess(){
+    panels.forEach(panel=>{
+        panel.classList.remove('active')
+    })
+}
